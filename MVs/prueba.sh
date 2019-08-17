6e53198f8c83b97404088256d1c71f6e3232289c
nomed1 <43537277+nomed1@users.noreply.github.com>
2019-08-17 16:09:29 +0200

    Delete prueba.sh

--- MVs/prueba.sh
+++
@@ -1,5 +0,0 @@
-sudo VBoxManage extpack install ~/Oracle_VM_VirtualBox_Extension_Pack-6.0.10.vbox-extpack
-VBoxManage hostonlyif create
-#ifup vboxnet0
-VBoxManage registervm ~/Escritorio/android44/android44.vbox
-
