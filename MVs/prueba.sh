2be31bed16bea5f123feb283ca1400caa96835cb
nomed1 <43537277+nomed1@users.noreply.github.com>
2019-08-17 16:02:39 +0200

    Add files via upload

---
+++ MVs/prueba.sh
@@ -0,0 +1,5 @@
+sudo VBoxManage extpack install ~/Oracle_VM_VirtualBox_Extension_Pack-6.0.10.vbox-extpack
+VBoxManage hostonlyif create
+#ifup vboxnet0
+VBoxManage registervm ~/Escritorio/android44/android44.vbox
+
